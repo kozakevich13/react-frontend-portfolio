f44ac1a1860bb5d30e683a2e27fcbf024ffff360
unknown <hanibalsmitti@gmail.com>
2023-03-07 16:37:58 +0300

    change logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 // import logo from '../assets/logo.svg' 
 const Navbar = () => {
+  const logo = "<vitaliydev/>"
   return (
     <nav className="bg-light navbar navbar-expand-lg shadow-sm p-4">
       <div className="container-fluid d-sm-flex flex-column flex-lg-row justify-content-center justify-content-lg-between">
         
         <div className="navbar-brand mb-4 mb-md-0 mb-lg-0">
           {/* <img src={logo} alt="logo" width="30" height="auto" className="d-inline-block align-text-top" /> */}
-          <span className="ms-2 fw-bold">vitaliydev</span>
+          <span className="ms-2 fw-bold">{logo}</span>
         </div>
         
         <div className="">
