cad4da387ad71517d6cfd42f5cfe15e4d26e0317
kozakevich13 <hanibalsmitti@gmail.com>
2023-04-19 18:02:47 +0300

    add two project pixel and birthdayCard

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 import windows98 from "./assets/projectImages/windows-98.png";
-import devTeam from "./assets/projectImages/dev-team.png"
+import devTeam from "./assets/projectImages/dev-team.png";
+import birthdayCard from "./assets/projectImages/birthday-card.png";
+import pixel from "./assets/projectImages/pixel.png"
 
 const ProjectsData = [
    
@@ -26,6 +28,30 @@ const ProjectsData = [
         "This page was created using such technologies as: React, Bootstrap, emailjs-com.",
     description_ua:  
         "Ця сторінка створена за допомогою таких техноголій як: React, Bootstrap, emailjs-com.",
+  },
+  {
+    id: "birthday-card",
+    img: birthdayCard,
+    name: "Birthday card",
+    stack: ["< CSS & HTML />", "< retoolapi.dev />", "< React.js />"],
+    live: "https://kozakevich13.github.io/birthday-card/",
+    source: "https://github.com/kozakevich13/birthday-card",
+    description_en:  
+        "This page uses retoolapi.dev to generate random birthday greetings.",
+    description_ua:  
+        "Ця сторінка використовує retoolapi.dev для генерації рандомних привітань з днем народження.",
+  },
+  {
+    id: "pixel",
+    img: pixel,
+    name: "Pixel",
+    stack: ["< Spline />", "< React.js />"],
+    live: "https://kozakevich13.github.io/pixel.github.io/",
+    source: "https://github.com/kozakevich13/pixel.github.io",
+    description_en:  
+        "This page was created with the help of such technologies as: Spline and React.js",
+    description_ua:  
+        "Ця сторінка створена за допомогою таких техноголій як: Spline і React.js",
   },
 
   
