37371021ecb25260549a1c6ad51bd5da0e717cc6
kozakevich13 <hanibalsmitti@gmail.com>
2023-04-19 11:54:14 +0300

    add new project dev-team

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 import windows98 from "./assets/projectImages/windows-98.png";
+import devTeam from "./assets/projectImages/dev-team.png"
 
 const ProjectsData = [
    
@@ -12,7 +13,19 @@ const ProjectsData = [
     description_en:  
         "This project was created to push myself in HTML and CSS3 and to learn more in depth React and react-grid-layout.",
     description_ua:  
-    "Цей проект був створений, щоб поглибити себе в HTML і CSS3 і дізнатися більше про React і react-grid-layout.",
+        "Цей проект був створений, щоб поглибити себе в HTML і CSS3 і дізнатися більше про React і react-grid-layout.",
+  },
+  {
+    id: "dev-team",
+    img: devTeam,
+    name: "Dev Team",
+    stack: ["< Bootstrap />", "< emailjs-com />", "< React.js />"],
+    live: "https://kozakevich13.github.io/dev-team/",
+    source: "https://github.com/kozakevich13/dev-team-landing",
+    description_en:  
+        "This page was created using such technologies as: React, Bootstrap, emailjs-com.",
+    description_ua:  
+        "Ця сторінка створена за допомогою таких техноголій як: React, Bootstrap, emailjs-com.",
   },
 
   
